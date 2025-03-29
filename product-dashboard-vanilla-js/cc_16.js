28dbb8ad47042681ae93f84d371159797689fa16
Payton <paytonsweet@usf.edu>
2025-03-29 14:26:53 -0400

    Task 3 - Add fetchProductsAsync using async/await and try/catch

--- product-dashboard-vanilla-js/cc_16.js
+++ product-dashboard-vanilla-js/cc_16.js
@@ -1,4 +1,4 @@
-// Task 2
+// Task 2: Fetch Products with .then()
 // sets api to a const
 const BASE_URL = "https://www.course-api.com/javascript-store-products";
 
@@ -19,6 +19,30 @@ function fetchProductsThen() { // function used to find product names
         throw error;
     });
 }
+
+// Task 3: Fetch Products with async/await
+async function fetchProductsAsync() { // fetches products using async
+    try {
+        const response = await fetch(BASE_URL); // waits before fetching url
+        if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`); // shows error if one is there
+        }
+        const products = await response.json();
+        displayProducts(products); // displays product using function
+    } catch (error) {
+        handleError(error); // logs errors using function
+    }
+}
+
+function displayProducts(products) { // function to display products
+    console.log("Displaying products:", products);
+}
+
+function handleError(error) { // function to log errors
+    console.error("Error fetching products:", error);
+}
+
+
 
 // calls function
 fetchProductsThen();
